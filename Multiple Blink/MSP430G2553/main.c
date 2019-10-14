129627257cde19ccdbae95d4ff46f14976e93a64
Prendergast1 <47788939+Prendergast1@users.noreply.github.com>
2019-10-14 14:51:07 -0400

    Add files via upload

--- Multiple Blink/MSP430G2553/main.c
+++ Multiple Blink/MSP430G2553/main.c
@@ -2,39 +2,58 @@
 // Code was initially started on September 18th, 2019
 // Code was last modified on October 5th, 2019
 // Submitted October 17th, 2019
-// Excercise: Button Blink for MSP430G2553
+// Excercise: Multiple Blink for MSP430G2553
+/* The objective in this exercise is essentially to write a program
+to allow two LEDs on the MSP430G2553 to blink on and off at different rates
+visible to the user.
+ */
 
 #include <msp430.h>
+#include <stdio.h>
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;       // Stop WDT (watch-dog timer)
-                                    // WDT is disabled as it is not needed for this program
+    WDTCTL = WDTPW | WDTHOLD;               // Stop WDT (watch-dog timer)
+                                            /* WDT (Line 16) automatically generates when
+                                            creating main.c file*/
+    // WDT is disabled as it is not needed for this program
 
+    // Configure GPIO (General-Purpose Input/Output)
 
-    P1DIR |= BIT0;  // Set P1.0 (Pin 1, Bit 0) as the output pin, P1.0 is LED1 on board
-    P1REN |= BIT3;  /* Enables internal resistor such that P1.3 (Pin 1, Bit 3)
-                     can be connected to 3.6 V (VCC) or ground (GND)*/
-    P1OUT |= BIT3;  /* Set P1.3 (Pin 1, Bit 3) as the input pin, in other words,
-                    the internal resistor is connected to 3.6 V (VCC) and functions as a pull-up resistor.*/
-                    // P1.3 basically represents the button on the MSP430G2553
+    /* For the MSP430G2553, LED1 is assigned to P1.0 (Pin 1, Bit 0)
+     * LED2 is assigned to P1.6 (Pin 1, Bit 6)
+     */
 
-    while(1)    // While loop
+    P1DIR |= BIT0;                         /* OR Pin 1 with Bit 0 to clear output latch
+                                            and set Bit 0 as output for Pin 1
+                                            */
+
+    P1OUT |= BIT0;                          // Set P1.0 to output direction, turning on LED1
+
+    P1DIR |= BIT6;                          /* OR Pin 1 with Bit 6 to clear output latch
+                                            and set Bit 6 as output for Pin 1
+                                            */
+
+    P1OUT |= BIT6;                          // Set P1.6 to output direction, turning on LED2
+
+    while(1) // Infinite While Loop
     {
-        // Once the program is ran, the LED is initially on and holds the value of 1.
-        // Once the button has been pressed once, the LED will toggle
-        // Once the button has been released, the LED will stop blinking on and off
-        // and remain on or off depending on when the user released the button
+        P1OUT ^= BIT0;                      /* Exclusive OR (^=) is utilized
+                                            to toggle LED1 between on and off*/
+        __delay_cycles(100000);
 
-        /* If-Statement is implemented such that if the value of Pin 1 input AND Bit 3 does not
-         * equal the value of bit 3, the LED will toggle.
-         */
+        P1OUT ^= BIT0;                      /* Exclusive OR (^=) is utilized
+                                            to toggle LED1 between on and off again
+                                            to increase the rate LED1 is being toggled*/
+        __delay_cycles(100000);             // The rate LED1 is being toggled at is doubled
 
-        if((P1IN & BIT3)!=BIT3)
-        {
-            P1OUT ^= BIT0;
-            __delay_cycles(100000);
-        }
+        P1OUT ^= BIT6;                      /* Exclusive OR (^=) is utilized
+                                            to toggle LED2 between on and off*/
+        __delay_cycles(100000);
+        /* Function to monitor the frequency the LED blinks,
+        this function delays the clock at a speed where the
+        LED is blinking at a pace that is visible to the user
+        */
+
     }
-
 }
